567566222e3f1ba80f0a5af7c5d48e2bb24eb784
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-03 17:34:22 +0200

    Add user input prompts for game selection and number entry

---
+++ Pari e Dispari/js/main.js
@@ -0,0 +1,14 @@
+// TODO L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+//* chiedo all'utente di scegliere Pari o Dispari con un prompt e salvo il tutto nel let userChoice, che stampo in console
+let userChoice = prompt("Seleziona Pari o Dispari")
+console.log(userChoice)
+
+//*chiedo all'utente di scegliere un numero da 1 a 5 con un prompt e salvo il tutto nel let userNumber, che stampo in console
+let userNumber = Number(prompt("Inserisci un numero intero da 1 a 5"))
+console.log(userChoice)
+ // * Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+
+
+
+ /* Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+ Dichiariamo chi ha vinto. */
