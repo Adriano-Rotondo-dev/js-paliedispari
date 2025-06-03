96f6868fb5b1da6612315dac41aba1ac5e94a29d
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-03 18:02:57 +0200

    Refactor main.js for improved readability and consistency in user input handling

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -6,37 +6,33 @@ console.log(userChoice);
 
 //*chiedo all'utente di scegliere un numero da 1 a 5 con un prompt e salvo il tutto nel let userNumber, che stampo in console
 let userNumber = Number(prompt("Inserisci un numero intero da 1 a 5"));
-console.log(userNumber) 
+console.log(userNumber);
 // * Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function botRNG() {
-    let rng = Math.floor(Math.random() *  5) + 1;
-    console.log(rng)
-    return rng;
+  let rng = Math.floor(Math.random() * 5) + 1;
+  console.log(rng);
+  return rng;
 }
-let botNumber = botRNG() // salvo la funzione in una variabile
+let botNumber = botRNG(); // salvo la funzione in una variabile
 
 // * Sommiamo i due numeri
 // * Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
-// * Dichiariamo chi ha vinto. 
+// * Dichiariamo chi ha vinto.
 
-let somma = userNumber + botNumber // salvo la somma dei numeri in una variabile
-console.log("La somma è " + somma) // stampo la somma in console
-if (userChoice === "pari" && (somma % 2 === 0) ) {
-    alert("La somma " + somma + " è pari! Hai vinto") 
-    console.log("Hai vinto")
-}
- else if (userChoice === "dispari" && (somma % 2 === 1)){
-    alert("La somma " + somma + " è dispari! Hai vinto") 
-    console.log("Hai vinto")
-}
- else if (userChoice === "dispari" && (somma % 2 === 0)){
-    alert("La somma " + somma + " è pari! Hai perso") 
-    console.log("Hai perso")
-}
- else if (userChoice === "pari" && (somma % 2 === 1)){
-    alert("La somma " + somma + " è dispari! Hai perso") 
-    console.log("Hai perso")
+let somma = userNumber + botNumber; // salvo la somma dei numeri in una variabile
+console.log("La somma è " + somma); // stampo la somma in console
+if (userChoice === "pari" && somma % 2 === 0) {
+  alert("La somma " + somma + " è pari! Hai vinto");
+  console.log("Hai vinto");
+} else if (userChoice === "dispari" && somma % 2 === 1) {
+  alert("La somma " + somma + " è dispari! Hai vinto");
+  console.log("Hai vinto");
+} else if (userChoice === "dispari" && somma % 2 === 0) {
+  alert("La somma " + somma + " è pari! Hai perso");
+  console.log("Hai perso");
+} else if (userChoice === "pari" && somma % 2 === 1) {
+  alert("La somma " + somma + " è dispari! Hai perso");
+  console.log("Hai perso");
 }
 /* else console.log ("Errore!")
 alert("Hai sbagliato qualcosa!") */
-
