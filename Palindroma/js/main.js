4aebdf9fda034116cb7d6f86a8e933ad6cb7f344
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-06-03 17:34:38 +0200

    Fix formatting and improve readability in palindrome function

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,25 +1,27 @@
 // TODO: creo un prompt per chiedere all'utente di inserire una parola
 
-let userWord = prompt("Scrivi una parola")
+let userWord = prompt("Scrivi una parola");
 //* stampo la parola inserita in console
-console.log(userWord)
+console.log(userWord);
 
 //TODO: creo una funzione che verifichi se la parola inserita è palindroma
 //* creo una funzione che legga al contrario la parola inserita , la salvo in una variabile e stampo in console il risultato
-function palindroma() {   
-    let reverseWord = "";
-    for (let i= userWord.length - 1; i >= 0; i --) { //ciclo all'interno della parola al contrario
-        const thisLetter = userWord[i]; 
-        console.log (thisLetter); // stampo le lettere ciclate
-        reverseWord = reverseWord + userWord[i]; //unisco le lettere ciclate nella variabile reverseWord
-    }
-    return reverseWord 
+function palindroma() {
+  let reverseWord = "";
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    //ciclo all'interno della parola al contrario
+    const thisLetter = userWord[i];
+    console.log(thisLetter); // stampo le lettere ciclate
+    reverseWord = reverseWord + userWord[i]; //unisco le lettere ciclate nella variabile reverseWord
+  }
+  return reverseWord;
 }
-let palindromaCheck = palindroma() //salvo la funzione in una variabile da richiamare
-console.log(palindroma(userWord)) // stampo la funzione applicata al prompt iniziale
+let palindromaCheck = palindroma(); //salvo la funzione in una variabile da richiamare
+console.log(palindroma(userWord)); // stampo la funzione applicata al prompt iniziale
 
-if(userWord === palindromaCheck) { //messaggio di alert una volta applicata la funzione
-    alert("La parola " + userWord + " è palindroma.")
+if (userWord === palindromaCheck) {
+  //messaggio di alert una volta applicata la funzione
+  alert("La parola " + userWord + " è palindroma.");
 } else {
-    alert("La parola " + userWord + " non è palindroma.")
+  alert("La parola " + userWord + " non è palindroma.");
 }
